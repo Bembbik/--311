30e1915cfb9a6716f50ef7853ac9935ce530c252
Bembbik <142221118+Bembbik@users.noreply.github.com>
2024-09-23 09:34:05 +0300

    Лабораторная 3 введение в специальность

---
+++ Лабораторная 3.py
@@ -0,0 +1,27 @@
+num = int(input())
+if num == 1:
+    print('Январь')
+elif num == 2:
+    print('Февраль')
+elif num == 3:
+    print('Март')
+elif num == 4:
+    print('Апрель')
+elif num == 5:
+    print('Май')
+elif num == 6:
+    print('Июнь')
+elif num == 7:
+    print('Июль')
+elif num == 8:
+    print('Август')
+elif num == 9:
+    print('Сентябрь')
+elif num == 10:
+    print('Октябрь')
+elif num == 11:
+    print('Ноябрь')
+elif num == 12:
+    print('Декабрь')
+elif num < 1 or num > 12:
+    print('Такого месяца нет!')
